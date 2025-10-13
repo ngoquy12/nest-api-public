9a6d11ee20a18334beb31335852202b969062e9b
ngovanquy <ngoq753429@gmail.com>
2025-10-13 21:32:29 +0700

    update API employee update

--- src/common/validators/index.ts
+++ src/common/validators/index.ts
@@ -1,6 +1,4 @@
-export * from './is-age-between-16-and-60.validator';
 export * from './is-before-today.validator';
 export * from './is-within-30-days.validator';
 export * from './no-only-spaces.validator';
 export * from './no-special-characters.decorator';
-export * from './time-compare.validator';
